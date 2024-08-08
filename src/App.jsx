38e56f47625f4350f9382845a5f9a50ea17fdf3f
JiyaGupta-cs <140608790+JiyaGupta-cs@users.noreply.github.com>
2024-08-08 21:50:38 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
